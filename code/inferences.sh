8cd0b2930604d8ce9f0f1b32ea76690aeb0228d6
Hohmann <n.h.hohmann@uu.nl>
2025-05-28 21:04:58 +0200

    update mcmc moves

--- code/inferences.sh
+++ code/inferences.sh
@@ -1,11 +1,12 @@
-SUFFIX1=full
-SUFFIX2=cont
-SUFFIX3=inc
-#rb_command1="morph <- readDiscreteCharacterData(\"data/char_mat_${SUFFIX1}.nex\"); taxa <- readTaxonData(\"data/taxa_${SUFFIX1}.tsv\"); outfile <- \"output/test_${SUFFIX1}.log\"; source(\"code/fbd_inference.rev\")"
-rb_command2="morph <- readDiscreteCharacterData(\"data/char_mat_${SUFFIX2}.nex\"); taxa <- readTaxonData(\"data/taxa_${SUFFIX2}.tsv\"); outfile <- \"output/test_${SUFFIX2}.log\"; source(\"code/fbd_inference.rev\")"
-rb_command3="morph <- readDiscreteCharacterData(\"data/char_mat_${SUFFIX3}.nex\"); taxa <- readTaxonData(\"data/taxa_${SUFFIX3}.tsv\"); outfile <- \"output/test_${SUFFIX3}.log\"; source(\"code/fbd_inference.rev\")"
+SUFFIX1=cont
+SUFFIX2=inc
+NO=2
 
-#echo $rb_command1 | "rb.exe" &
+rb_command1="morph <- readDiscreteCharacterData(\"data/char_mat_${SUFFIX1}.nex\"); taxa <- readTaxonData(\"data/taxa_${SUFFIX1}.tsv\"); outfile <- \"output/test_${SUFFIX1}_${NO}.log\"; treefile_name <- \"output/trees_${SUFFIX1}_${NO}.log\"; source(\"code/fbd_inference.rev\")"
+rb_command2="morph <- readDiscreteCharacterData(\"data/char_mat_${SUFFIX2}.nex\"); taxa <- readTaxonData(\"data/taxa_${SUFFIX2}.tsv\"); outfile <- \"output/test_${SUFFIX2}_${NO}.log\"; treefile_name <- \"output/trees_${SUFFIX2}_${NO}.log\";source(\"code/fbd_inference.rev\")"
+rb_command3="morph <- readDiscreteCharacterData(\"data/char_mat_${SUFFIX2}.nex\"); taxa <- readTaxonData(\"data/taxa_${SUFFIX2}.tsv\"); outfile <- \"output/test_skyline_${NO}.log\"; treefile_name <- \"output/trees_skyline_${NO}.log\"; source(\"code/skyline_inference.rev\")"
+
+echo $rb_command1 | "rb.exe" &
 echo $rb_command2 | "rb.exe" &
 echo $rb_command3 | "rb.exe" &
 wait
