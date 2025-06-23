b88f4b8281adafb53b9d44a584b1a5d9bb1dfd64
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-06-23 17:13:14 +0200

    fix naming

--- code/inference_base_A.sh
+++ code/inference_base_A.sh
@@ -2,6 +2,6 @@ ID=$1
 NCHAR=$2
 INS="inc_A"
 
-rb_command="morph <- readDiscreteCharacterData(\"data/sim/char_mat_${INS}_rho0_nchar${NCHAR}_${ID}.nex\"); taxa <- readTaxonData(\"data/sim/fossils_${INS}_rho0_${ID}.tsv\"); outfile <- \"output/res_base_${NCHAR}_${ID}.log\"; treefile_name <- \"output/trees_base_${NCHAR}_${ID}.log\"; source(\"code/fbd_inference.rev\")"
+rb_command="morph <- readDiscreteCharacterData(\"data/sim/char_mat_${INS}_rho0_nchar${NCHAR}_${ID}.nex\"); taxa <- readTaxonData(\"data/sim/fossils_${INS}_rho0_${ID}.tsv\"); outfile <- \"output/res_base_${INC}_${NCHAR}_${ID}.log\"; treefile_name <- \"output/trees_base__${INS}_${NCHAR}_${ID}.log\"; source(\"code/fbd_inference.rev\")"
 
 echo $rb_command | "rb.exe"
