017a410f8f59fd96d9f9c73cd6acbed122957a9f
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-07-02 15:35:23 +0200

    minor changes

--- code/inferences.sh
+++ code/inferences.sh
@@ -1,12 +1,8 @@
-SUFFIX1=cont
-SUFFIX2=inc
-NO=3
 
-rb_command1="morph <- readDiscreteCharacterData(\"data/char_mat_${SUFFIX1}.nex\"); taxa <- readTaxonData(\"data/taxa_${SUFFIX1}.tsv\"); outfile <- \"output/test_${SUFFIX1}_${NO}.log\"; treefile_name <- \"output/trees_${SUFFIX1}_${NO}.log\"; source(\"code/fbd_inference.rev\")"
-rb_command2="morph <- readDiscreteCharacterData(\"data/char_mat_${SUFFIX2}.nex\"); taxa <- readTaxonData(\"data/taxa_${SUFFIX2}.tsv\"); outfile <- \"output/test_${SUFFIX2}_${NO}.log\"; treefile_name <- \"output/trees_${SUFFIX2}_${NO}.log\";source(\"code/fbd_inference.rev\")"
-rb_command3="morph <- readDiscreteCharacterData(\"data/char_mat_${SUFFIX2}.nex\"); taxa <- readTaxonData(\"data/taxa_${SUFFIX2}.tsv\"); outfile <- \"output/test_skyline_${NO}.log\"; treefile_name <- \"output/trees_skyline_${NO}.log\"; source(\"code/skyline_inference.rev\")"
-
-echo $rb_command1 | "rb.exe" &
-echo $rb_command2 | "rb.exe" &
-echo $rb_command3 | "rb.exe" &
+bash code/inference_skyline_A.sh 1 30 &
+bash code/inference_skyline_A.sh 2 30 &
+bash code/inference_skyline_A.sh 3 30 &
+bash code/inference_skyline_A.sh 4 30 &
+bash code/inference_skyline_A.sh 5 30 &
+bash code/inference_skyline_A.sh 6 30 &
 wait
