fffe3aef280f123996b9ff20dc3a84b61c7f079e
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-07-03 10:29:00 +0200

    isolate model components from inference

--- code/inferences.sh
+++ code/inferences.sh
@@ -1,8 +1,8 @@
 
-bash code/inference_skyline_A.sh 1 30 &
-bash code/inference_skyline_A.sh 2 30 &
-bash code/inference_skyline_A.sh 3 30 &
-bash code/inference_skyline_A.sh 4 30 &
-bash code/inference_skyline_A.sh 5 30 &
-bash code/inference_skyline_A.sh 6 30 &
+bash code/inference_base.sh 1 30 &
+bash code/inference_base.sh 2 30 &
+bash code/inference_base.sh 3 30 &
+bash code/inference_base.sh 4 30 &
+bash code/inference_base.sh 5 30 &
+bash code/inference_base.sh 6 30 &
 wait
