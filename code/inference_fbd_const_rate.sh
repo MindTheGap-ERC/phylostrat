9684c31d0fd3317ee982f399da07dc58d9d15602
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-07-03 17:57:49 +0200

    doodles with snakefile

---
+++ code/inference_fbd_const_rate.sh
@@ -0,0 +1,8 @@
+INPUT_CHAR=$1
+INPUT_FOSSILS=$2
+OUTPUT_NUMERIC=$3
+OUTPUT_TREES=$4
+
+rb_command="morph <- readDiscreteCharacterData(\"${INPUT_CHAR}\"); taxa <- readTaxonData(\"${INPUT_FOSSILS}\"); outfile <- \"${OUTPUT_NUMERIC}\"; treefile_name <- \"${OUTPUT_TREES}\"; source(\"code/fbd_inference.rev\")"
+echo $rb_command
+echo $rb_command | "rb.exe"
