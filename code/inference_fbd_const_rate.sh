a0a230eeefca6ec7e6806f4767a3f0593b18c246
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-07-04 17:09:34 +0200

    end of day commit

--- code/inference_fbd_const_rate.sh
+++ code/inference_fbd_const_rate.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
+source ~/.bashrc
 INPUT_CHAR=$1
 INPUT_FOSSILS=$2
 OUTPUT_NUMERIC=$3
 OUTPUT_TREES=$4
-
-rb_command="morph <- readDiscreteCharacterData(\"${INPUT_CHAR}\"); taxa <- readTaxonData(\"${INPUT_FOSSILS}\"); outfile <- \"${OUTPUT_NUMERIC}\"; treefile_name <- \"${OUTPUT_TREES}\"; source(\"code/fbd_inference.rev\")"
-echo $rb_command
-echo $rb_command | "rb.exe"
+NRUNS=$5
+rb_command="morph <- readDiscreteCharacterData(\"${INPUT_CHAR}\"); taxa <- readTaxonData(\"${INPUT_FOSSILS}\"); outfile <- \"${OUTPUT_NUMERIC}\"; treefile_name <- \"${OUTPUT_TREES}\"; nruns <- \"${NRUNS}\"; source(\"code/fbd_inference.rev\")"
+echo $rb_command | "rb"
--- code/inference_test.sh
+++ code/inference_test.sh
@@ -1,14 +1,1 @@
-INPUT_CHAR=$1
-INPUT_FOSSILS=$2
-OUTPUT_NUMERIC=$3
-OUTPUT_TREES=$4
-#NCHAR=$2
-#OUTPUT_TREE_FILE=$3
-#INS="cont"
-#echo $ID
-#OUTPUT_NUMERIC_FILE="output/res_base_${INS}_${ID}_${NCHAR}.log"
-#echo $OUTPUT_NUMERIC_FILE
-#echo $INS
-#rb_command="morph <- readDiscreteCharacterData(\"data/sim/char_mat_${INS}_rho0_nchar${NCHAR}_${ID}.nex\"); taxa <- readTaxonData(\"data/sim/fossils_${INS}_rho0_${ID}.tsv\"); outfile <- \"${OUTPUT_NUMERIC_FILE}\"; treefile_name <- \"${OUTPUT_TREE_FILE}\"; source(\"code/fbd_inference.rev\")"
-rb_command="morph <- readDiscreteCharacterData(\"${INPUT_CHAR}\"); taxa <- readTaxonData(\"${INPUT_FOSSILS}\"); outfile <- \"${OUTPUT_NUMERIC}\"; treefile_name <- \"${OUTPUT_TREES}\"; source(\"code/fbd_inference.rev\")"
-echo $rb_command #| "rb.exe"
+bash code/inference_fbd_skyline_A.sh data/sim/char_mat_inc_A_rho0_nchar30_1.nex data/sim/fossils_inc_A_rho0_1.tsv output/res_skyline_A_1_30.log output/trees_skyline_A_1_30.log
