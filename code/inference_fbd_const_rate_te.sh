18a30a23923ac5c554729fb78610d21158ef7fe4
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-07-15 10:05:58 +0200

    add snakemake for total evidence analysis

---
+++ code/inference_fbd_const_rate_te.sh
@@ -0,0 +1,9 @@
+INPUT_MORPH=$1
+INPUT_TAXA=$2
+INPUT_MOL=$3
+OUTPUT_NUMERIC=$4
+OUTPUT_TREES=$5
+
+RB_COMMAND="morph <- readDiscreteCharacterData(\"${INPUT_MORPH}\"); taxa <- readTaxonData(\"${INPUT_TAXA}\"); mol <- readDiscreteCharacterData(\"${INPUT_MOL}\"); outfile <- \"${OUTPUT_NUMERIC}\"; treefile <- \"${OUTPUT_TREES}\"; source(\"code/fbd_inference_te.rev\")" 
+
+echo $RB_COMMAND | "rb"
