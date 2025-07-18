ef220e6c15d65b9dbf6d6e8b980809adb34fa436
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-07-18 10:47:16 +0200

    generate model DAG, correct var names

--- code/test.sh
+++ code/test.sh
@@ -1,3 +1,9 @@
 #bash code/inference_fbd_const_rate_te.sh data/sim/char_mat_cont_rho1_nchar30_1.nex data/sim/fossils_cont_rho1_1.tsv data/sim/mol_data_1.nex output/test1.log output/test_tree1.log
+#bash code/inference_fbd_skyline_A.sh data/sim/char_mat_inc_A_rho0_nchar30_1.nex data/sim/fossils_inc_A_rho0_1.tsv output/res_skyline_A_1_30.log output/trees_skyline_A_1_30.log
+#bash code/inference_fbd_skyline_A_te.sh data/sim/char_mat_cont_rho1_nchar30_1.nex data/sim/fossils_cont_rho1_1.tsv data/sim/mol_data_1.nex output/test1.log output/test_tree1.log
 
-bash code/inference_fbd_skyline_A_te.sh data/sim/char_mat_cont_rho1_nchar30_1.nex data/sim/fossils_cont_rho1_1.tsv data/sim/mol_data_1.nex output/test1.log output/test_tree1.log
+#bash code/inference_fbd_fo.sh data/sim/char_mat_inc_A_rho0_nchar30_1.nex data/sim/fossils_inc_A_rho0_1.tsv output/res_skyline_A_1_30.log output/trees_skyline_A_1_30.log
+
+#bash code/inference_fbd_skyline_A_fo.sh data/sim/char_mat_inc_A_rho0_nchar30_1.nex data/sim/fossils_inc_A_rho0_1.tsv output/res_skyline_A_1_30.log output/trees_skyline_A_1_30.log
+
+#bash code/inference_fbd_te.sh data/sim/char_mat_cont_rho1_nchar30_1.nex data/sim/fossils_cont_rho1_1.tsv data/sim/mol_data_1.nex output/test1.log output/test_tree1.log
