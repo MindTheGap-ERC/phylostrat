3579322c0ff6dcdbef3704738840cfdf9df2a3be
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-07-18 09:43:56 +0200

    delete prototyping code

--- code/inferences.sh
+++
@@ -1,8 +0,0 @@
-
-bash code/inference_base.sh 1 30 &
-bash code/inference_base.sh 2 30 &
-bash code/inference_base.sh 3 30 &
-bash code/inference_base.sh 4 30 &
-bash code/inference_base.sh 5 30 &
-bash code/inference_base.sh 6 30 &
-wait
