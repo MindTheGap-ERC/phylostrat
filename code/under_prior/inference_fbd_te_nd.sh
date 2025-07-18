64b09dc137226db57c6fb60fa2116d739cf80a04
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-07-18 13:13:46 +0200

    analysis under prior

---
+++ code/under_prior/inference_fbd_te_nd.sh
@@ -0,0 +1,10 @@
+# fbd inference - total evidence constant rates
+INPUT_MORPH=$1
+INPUT_TAXA=$2
+INPUT_MOL=$3
+OUTPUT_NUMERIC=$4
+OUTPUT_TREES=$5
+
+RB_COMMAND="morph <- readDiscreteCharacterData(\"${INPUT_MORPH}\"); taxa <- readTaxonData(\"${INPUT_TAXA}\"); mol <- readDiscreteCharacterData(\"${INPUT_MOL}\"); outfile <- \"${OUTPUT_NUMERIC}\"; treefile <- \"${OUTPUT_TREES}\"; source(\"code/under_prior/fbd_inference_te_nd.rev\")" 
+
+echo $RB_COMMAND | "rb"
