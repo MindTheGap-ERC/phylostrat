4096a69ed81aeb8f88e3ba164f36cda257e3b11c
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-07-18 09:25:36 +0200

    unify naming conventions, set priors to exp. values

--- code/inference_fbd_const_rate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-INPUT_CHAR=$1
-INPUT_FOSSILS=$2
-OUTPUT_NUMERIC=$3
-OUTPUT_TREES=$4
-rb_command="morph <- readDiscreteCharacterData(\"${INPUT_CHAR}\"); taxa <- readTaxonData(\"${INPUT_FOSSILS}\"); outfile <- \"${OUTPUT_NUMERIC}\"; treefile_name <- \"${OUTPUT_TREES}\"; source(\"code/fbd_inference.rev\")"
-echo $rb_command | "rb"
