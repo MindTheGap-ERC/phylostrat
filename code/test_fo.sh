8bc90fad5e0cac72da3145cf987a03271bcf9f1a
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-07-31 17:01:16 +0200

    modify priors, add fo analysis

--- code/test_fo.sh
+++ code/test_fo.sh
@@ -6,8 +6,8 @@
 
 #bash code/inference_fbd_skyline_A_fo.sh data/sim/char_mat_inc_A_rho0_nchar30_1.nex data/sim/fossils_inc_A_rho0_1.tsv output/res_skyline_A_1_30.log output/trees_skyline_A_1_30.log
 
-bash code/inference_fbd_te.sh data/sim/char_mat_cont_rho1_nchar30_1.nex data/sim/fossils_cont_rho1_1.tsv data/sim/mol_data_1.nex output/num_test_te_cont.log output/tree_test_te_cont.log &
-bash code/inference_fbd_te.sh data/sim/char_mat_inc_A_rho1_nchar30_1.nex data/sim/fossils_inc_A_rho1_1.tsv data/sim/mol_data_1.nex output/num_test_te_inc_A.log output/tree_test_te_inc_A.log &
-bash code/inference_fbd_te.sh data/sim/char_mat_inc_B_rho1_nchar30_1.nex data/sim/fossils_inc_B_rho1_1.tsv data/sim/mol_data_1.nex output/num_test_te_inc_B.log output/tree_test_te_inc_B.log &
-bash code/inference_fbd_skyline_A_te.sh data/sim/char_mat_inc_A_rho1_nchar30_1.nex data/sim/fossils_inc_A_rho1_1.tsv data/sim/mol_data_1.nex output/num_test_te_skyline_A.log output/tree_test_te_skyline_A.log &
+bash code/inference_fbd_fo.sh data/sim/char_mat_cont_rho0_nchar30_1.nex data/sim/fossils_cont_rho0_1.tsv output/num_test_fo_cont.log output/tree_test_fo_cont.log &
+bash code/inference_fbd_fo.sh data/sim/char_mat_inc_A_rho0_nchar30_1.nex data/sim/fossils_inc_A_rho0_1.tsv output/num_test_fo_inc_A.log output/tree_test_fo_inc_A.log &
+bash code/inference_fbd_fo.sh data/sim/char_mat_inc_B_rho0_nchar30_1.nex data/sim/fossils_inc_B_rho0_1.tsv output/num_test_fo_inc_B.log output/tree_test_fo_inc_B.log &
+bash code/inference_fbd_skyline_A_fo.sh data/sim/char_mat_inc_A_rho_nchar30_1.nex data/sim/fossils_inc_A_rho0_1.tsv output/num_test_fo_skyline_A.log output/tree_test_fo_skyline_A.log &
 wait
