875f787937dc83ea456bc5ab98ba7f41bd2865d5
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-08-02 13:57:05 +0200

    fix typo

--- code/inference_fbd_skyline_A_full_te.sh
+++ code/inference_fbd_skyline_A_full_te.sh
@@ -4,6 +4,6 @@ INPUT_MOL=$3
 OUTPUT_NUMERIC=$4
 OUTPUT_TREES=$5
 
-RB_COMMAND="morph <- readDiscreteCharacterData(\"${INPUT_MORPH}\"); taxa <- readTaxonData(\"${INPUT_TAXA}\"); mol <- readDiscreteCharacterData(\"${INPUT_MOL}\"); outfile <- \"${OUTPUT_NUMERIC}\"; treefile <- \"${OUTPUT_TREES}\"; source(\"code/fbd_inference_skyline_A_te.rev\")" 
+RB_COMMAND="morph <- readDiscreteCharacterData(\"${INPUT_MORPH}\"); taxa <- readTaxonData(\"${INPUT_TAXA}\"); mol <- readDiscreteCharacterData(\"${INPUT_MOL}\"); outfile <- \"${OUTPUT_NUMERIC}\"; treefile <- \"${OUTPUT_TREES}\"; source(\"code/fbd_inference_skyline_A_full_te.rev\")" 
 
 echo $RB_COMMAND | "rb"
