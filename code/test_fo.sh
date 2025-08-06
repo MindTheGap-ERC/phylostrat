fc1930a45eb45901a81fb5f1809d830e2f0bfeda
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-08-06 13:03:33 +0200

    massage te analysis

--- code/test_fo.sh
+++ code/test_fo.sh
@@ -6,9 +6,9 @@
 
 #bash code/inference_fbd_skyline_A_fo.sh data/sim/char_mat_inc_A_rho0_nchar30_1.nex data/sim/fossils_inc_A_rho0_1.tsv output/res_skyline_A_1_30.log output/trees_skyline_A_1_30.log
 
-bash code/inference_fbd_fo.sh data/sim/char_mat_cont_rho0_nchar30_1.nex data/sim/fossils_cont_rho0_1.tsv output/num_test_fo_cont.log output/tree_test_fo_cont.log &
-bash code/inference_fbd_fo.sh data/sim/char_mat_inc_A_rho0_nchar30_1.nex data/sim/fossils_inc_A_rho0_1.tsv output/num_test_fo_inc_A.log output/tree_test_fo_inc_A.log &
-bash code/inference_fbd_fo.sh data/sim/char_mat_inc_B_rho0_nchar30_1.nex data/sim/fossils_inc_B_rho0_1.tsv output/num_test_fo_inc_B.log output/tree_test_fo_inc_B.log &
-bash code/inference_fbd_skyline_A_fo.sh data/sim/char_mat_inc_A_rho0_nchar30_1.nex data/sim/fossils_inc_A_rho0_1.tsv output/num_test_fo_skyline_A.log output/tree_test_fo_skyline_A.log &
-bash code/inference_fbd_skyline_A_full_fo.sh data/sim/char_mat_inc_A_rho0_nchar30_1.nex data/sim/fossils_inc_A_rho0_1.tsv output/num_test_fo_skyline_A_full.log output/tree_test_fo_skyline_A_full.log &
+bash code/inference_fbd_fo.sh data/sim/char_mat_cont_rho0_nchar100_1.nex data/sim/fossils_cont_rho0_1.tsv output/num_test_fo_cont.log output/tree_test_fo_cont.log &
+bash code/inference_fbd_fo.sh data/sim/char_mat_inc_A_rho0_nchar100_1.nex data/sim/fossils_inc_A_rho0_1.tsv output/num_test_fo_inc_A.log output/tree_test_fo_inc_A.log &
+bash code/inference_fbd_fo.sh data/sim/char_mat_inc_B_rho0_nchar100_1.nex data/sim/fossils_inc_B_rho0_1.tsv output/num_test_fo_inc_B.log output/tree_test_fo_inc_B.log &
+bash code/inference_fbd_skyline_A_fo.sh data/sim/char_mat_inc_A_rho0_nchar100_1.nex data/sim/fossils_inc_A_rho0_1.tsv output/num_test_fo_skyline_A.log output/tree_test_fo_skyline_A.log &
+bash code/inference_fbd_skyline_A_full_fo.sh data/sim/char_mat_inc_A_rho0_nchar100_1.nex data/sim/fossils_inc_A_rho0_1.tsv output/num_test_fo_skyline_A_full.log output/tree_test_fo_skyline_A_full.log &
 wait
