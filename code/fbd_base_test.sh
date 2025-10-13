4d0b5d8ce60b7e407f723584630f41f34475f5c6
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2025-10-13 13:40:46 +0200

    basic fbd te implementation

---
+++ code/fbd_base_test.sh
@@ -0,0 +1,3 @@
+ID=1
+NCHAR=30
+bash code/inference_fbd_base.sh data/sim/fbd_base/char_mat_${ID}_nchar${NCHAR}.nex data/sim/fbd_base/fossils_${ID}_nchar${NCHAR}.csv data/sim/fbd_base/tree_${ID}_nchar${NCHAR}.nex output/fbd_base/num_test_${ID}_${NCHAR}_ output/fbd_base/tree_test_${ID}_${NCHAR}_ data/sim/fbd_base/mol_dat_${ID}_nchar${NCHAR}.nex
