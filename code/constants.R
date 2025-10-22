eefec5d8d52b33f45bacd92842df10e52584e2fa
Niklas Hohmann <n.h.hohmann@uu.nl>
2025-10-22 07:42:06 +0200

    remove loading of adms

--- code/constants.R
+++ code/constants.R
@@ -22,7 +22,7 @@ mod = "-mHKY -t5 -a0.25 -g5" # HKY model, 5 categories for the gamma distributio
 ids = seq_len(n_rep) # ids for the replicates
 
 #### Stratigraphic context ####
-miller_data = read.csv("data/strat/miller_2020_adm.csv")
-sinusoid_data = read.csv("data/strat/sinusoid_adm.csv")
-adm_miller = tp_to_adm(t = miller_data$time..Myr.- min(miller_data$time..Myr.), h = miller_data$adm_3..m.)
-adm_sinusoid = tp_to_adm(t = sinusoid_data$time..Myr., h = sinusoid_data$adm_1..m.)
+#miller_data = read.csv("data/strat/miller_2020_adm.csv")
+#sinusoid_data = read.csv("data/strat/sinusoid_adm.csv")
+#adm_miller = tp_to_adm(t = miller_data$time..Myr.- min(miller_data$time..Myr.), h = miller_data$adm_3..m.)
+#adm_sinusoid = tp_to_adm(t = sinusoid_data$time..Myr., h = sinusoid_data$adm_1..m.)
