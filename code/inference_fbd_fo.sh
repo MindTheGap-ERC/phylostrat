ee19f987c0c183f53c7bedf0a9fb26ca1b776703
Niklas Hohmann <n.h.hohmann@uu.nl>
2025-10-22 09:06:30 +0200

    delete fo inference

--- code/inference_fbd_fo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# fossil only fbd inference - constant rates
-set -euo pipefail
-INPUT_CHAR=$1
-INPUT_FOSSILS=$2
-OUTPUT_NUMERIC=$3
-OUTPUT_TREES=$4
-rb_command="morph <- readDiscreteCharacterData(\"${INPUT_CHAR}\"); taxa <- readTaxonData(\"${INPUT_FOSSILS}\"); outfile <- \"${OUTPUT_NUMERIC}\"; treefile_name <- \"${OUTPUT_TREES}\"; source(\"code/fbd_inference_fo.rev\")"
-echo $rb_command | "rb"
