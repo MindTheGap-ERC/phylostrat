3bfbb1e7040c41ac6923c84ab445bda39b88c10a
Niklas Hohmann <n.h.hohmann@uu.nl>
2025-10-22 09:06:50 +0200

    update naming convention for test scripts

--- code/test_fbd_strat.sh
+++ code/test_fbd_strat.sh
@@ -1,4 +1,4 @@
 ID=1
 NCHAR=30
-CASE=miller
+CASE=sinusoid
 bash code/inference_fbd_base.sh data/sim/fbd_strat/char_mat_${ID}_nchar${NCHAR}_${CASE}.nex data/sim/fbd_strat/fossils_${ID}_nchar${NCHAR}_${CASE}.csv data/sim/fbd_strat/tree_${ID}_nchar${NCHAR}_${CASE}.nex output/fbd_strat/num_test_${ID}_${NCHAR}_${CASE} output/fbd_strat/tree_test_${ID}_${NCHAR}_${CASE} data/sim/fbd_strat/mol_dat_${ID}_nchar${NCHAR}_${CASE}.nex
