e10ecb84a433dc89c74e1379ddd9008afbc4f3ed
Niklas Hohmann <n.h.hohmann@uu.nl>
2025-10-29 09:16:17 +0100

    fix file path

--- code/fetch_data_gap_prior.sh
+++ code/fetch_data_gap_prior.sh
@@ -1,2 +1,2 @@
-ibridges download "irods://nluu11p/home/research-mindthegap/phylostrat/data/sim/fbd_gap_est" "data/sim/" --overwrite
-ibridges download "irods://nluu11p/home/research-mindthegap/phylostrat/output/fbd_gap_est" "output/" --overwrite
+ibridges download "irods://nluu11p/home/research-mindthegap/phylostrat/data/sim/fbd_gap_prior" "data/sim/" --overwrite
+ibridges download "irods://nluu11p/home/research-mindthegap/phylostrat/output/fbd_gap_prior" "output/" --overwrite
