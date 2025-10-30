e0867a9556f31164e50ec0022289d43642395770
Niklas Hohmann <n.h.hohmann@uu.nl>
2025-10-30 14:56:29 +0100

    new test cases

--- code/test_fbd_base.sh
+++ code/test_fbd_base.sh
@@ -1,3 +1,3 @@
 ID=1
 NCHAR=30
-bash code/inference_fbd_base.sh data/sim/fbd_base/char_mat_${ID}_nchar${NCHAR}.nex data/sim/fbd_base/fossils_${ID}_nchar${NCHAR}.csv data/sim/fbd_base/tree_${ID}_nchar${NCHAR}.nex output/fbd_base/num_test_${ID}_${NCHAR}_ output/fbd_base/tree_test_${ID}_${NCHAR}_ data/sim/fbd_base/mol_dat_${ID}_nchar${NCHAR}.nex
+bash code/inference_fbd_base.sh sim_data/fbd_base/char_mat_${ID}_nchar${NCHAR}.nex sim_data/fbd_base/fossils_${ID}_nchar${NCHAR}.csv sim_data/fbd_base/tree_${ID}_nchar${NCHAR}.nex rb_output/fbd_base/num_test_${ID}_${NCHAR}_ rb_output/fbd_base/tree_test_${ID}_${NCHAR}_ sim_data/fbd_base/mol_dat_${ID}_nchar${NCHAR}.nex
