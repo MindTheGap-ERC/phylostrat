3c4e886b629d6f31064070ee4e5f6afd444434c6
Niklas Hohmann <n.h.hohmann@uu.nl>
2025-12-11 13:20:47 +0100

    add local data storage

--- code/test_fbd_gap_est.sh
+++ code/test_fbd_gap_est.sh
@@ -1,4 +1,4 @@
 ID=1
 NCHAR=30
 CASE=sinusoid # don't change, only sinusoid data makes sense for this analysis
-bash code/inference_fbd_gap_est.sh sim_data/fbd_gap_est/char_mat_${ID}_nchar${NCHAR}_${CASE}.nex sim_data/fbd_gap_est/fossils_${ID}_nchar${NCHAR}_${CASE}.csv rb_output/fbd_gap_est/num_test_${ID}_${NCHAR}_${CASE} rb_output/fbd_gap_est/tree_test_${ID}_${NCHAR}_${CASE} sim_data/fbd_gap_est/mol_dat_${ID}_nchar${NCHAR}_${CASE}.nex
+bash code/inference_fbd_gap_est.sh data/fbd_gap_est/sim/char_mat_${ID}_nchar${NCHAR}_${CASE}.nex data/fbd_gap_est/sim/fossils_${ID}_nchar${NCHAR}_${CASE}.csv data/fbd_gap_est/rb_output/num_test_${ID}_${NCHAR}_${CASE} data/fbd_gap_est/rb_output/tree_test_${ID}_${NCHAR}_${CASE} data/fbd_gap_est/sim/mol_dat_${ID}_nchar${NCHAR}_${CASE}.nex
